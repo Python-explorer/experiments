0ba0cfae3ed2b49573ec416ffef6fcdc7e45f350
Python_explorer <70327477+Python-explorer@users.noreply.github.com>
2023-11-07 23:54:22 +0000

    Update electives.py

--- electives.py
+++ electives.py
@@ -6,27 +6,22 @@ from io import BytesIO
 from streamlit import exception as st_exception
 
 # Function to load data
-@st.cache
+@st.experimental_memo  # Updated from st.cache to st.experimental_memo
 def load_data(url):
     try:
-        # Use pandas to read the excel file from the given url
-        return pd.read_excel(url)
-  
+        # Use pandas to read the CSV file from the given url
+        return pd.read_csv(url)
+    except Exception as e:
+        # If there's an error, show it in the Streamlit app
+        st.error(f"An error occurred while loading the data: {e}")
+        return pd.DataFrame()  # Return an empty DataFrame as a fallback
 
-# Specify the URL of your Excel file
-data_url = 'https://docs.google.com/spreadsheets/d/1zCrX-_4zPRxXuqHrKwnJ_uLwaU-iH_zVMy65Q7IsO1M/edit?usp=sharing'
+# Specify the URL of your CSV file
+data_url = 'https://raw.githubusercontent.com/yourusername/yourrepo/main/yourfile.csv'
 
 # Make sure to call load_data with the data_url
 data = load_data(data_url)
 
-# Check if data is loaded and not empty before proceeding
-if not data.empty:
-    # Get the unique values from the 'Treatment Function' column as options for the selectbox
-    unique_functions = data['Treatment Function'].unique().tolist()
-    selected_function = st.sidebar.selectbox('Select a Treatment Function:', unique_functions)
-else:
-    # If data is empty, display a message in the sidebar
-    st.sidebar.write("No data available.")
 
 # Function to plot the bar chart
 def plot_bar_chart(data, selected_function):
