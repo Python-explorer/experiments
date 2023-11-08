2d2a00a4fdc147c05fd90526b8ed1d348a1e5084
Python_explorer <70327477+Python-explorer@users.noreply.github.com>
2023-11-08 21:18:21 +0000

    Update electives.py

--- electives.py
+++ electives.py
@@ -73,8 +73,8 @@ for i, (icb_name, value) in enumerate(zip(df_sorted['ICB Name'], df_sorted[selec
     color = 'blue' if icb_name == selected_icb_focus and selected_icb_focus != 'None' else colors[i]
     ax.bar(icb_name, value, color=color)
 
-ax.set_xlabel('ICB Name')
-ax.set_ylabel(selected_value_column)
+ax.set_xlabel('ICB Name', fontsize=14)
+ax.set_ylabel(selected_value_column, fontsize=14)
 ax.set_title(f'{selected_value_column} by ICB Name for {selected_treatment_function}')
 plt.xticks(rotation=45, ha='right')  # Adjust the rotation and alignment of x-axis labels
 
