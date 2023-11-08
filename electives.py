5bd5ee71c6afb6d3431c6fd5adef5060f296f16c
Python_explorer <70327477+Python-explorer@users.noreply.github.com>
2023-11-08 01:07:27 +0000

    Update electives.py

--- electives.py
+++ electives.py
@@ -9,34 +9,30 @@ csv_url = 'https://raw.githubusercontent.com/Python-explorer/experiments/main/El
 def load_data(url):
     # Load the CSV data into a pandas DataFrame
     data = pd.read_csv(url)
-  
+    # Convert 'Total number of incomplete pathways' to numeric, just in case it's not
+    data['Total number of incomplete pathways'] = pd.to_numeric(data['Total number of incomplete pathways'], errors='coerce')
+    return data
 
 # Load the data
 df = load_data(csv_url)
 
-# Verify that we have numeric data and there's variation in the 'Total number of incomplete pathways' column
-if not pd.api.types.is_numeric_dtype(df['Total number of incomplete pathways']):
-    st.error('The "Total number of incomplete pathways" column does not contain numeric data.')
-else:
-    # Streamlit dropdown menu for 'Treatment Function'
-    selected_treatment = st.selectbox(
-        'Select a Treatment Function:',
-        options=df['Treatment Function'].unique()
-    )
+# Filter the DataFrame for the given 'ICB NAME'
+icb_name_filter = 'NHS SUSSEX INTEGRATED CARE BOARD'
+df_filtered = df[df['ICB Name'] == icb_name_filter]
 
-    # Filter the DataFrame based on the selected treatment function
-    filtered_df = df[df['Treatment Function'] == selected_treatment]
+# Group by 'Treatment Function' and sum 'Total number of incomplete pathways'
+df_grouped = df_filtered.groupby('Treatment Function')['Total number of incomplete pathways'].sum().reset_index()
 
-    # Sort the filtered DataFrame based on 'Total number of incomplete pathways' in descending order
-    sorted_df = filtered_df.sort_values(by='Total number of incomplete pathways', ascending=False)
+# Sort the grouped data by 'Total number of incomplete pathways' in ascending order for the bar chart
+df_sorted = df_grouped.sort_values(by='Total number of incomplete pathways', ascending=True)
 
-    # Create a vertical bar chart using Matplotlib
-    fig, ax = plt.subplots(figsize=(10, 6))
-    ax.bar(sorted_df['ICB Name'], sorted_df['Total number of incomplete pathways'])
-    ax.set_xlabel('ICB Name')
-    ax.set_ylabel('Total number of incomplete pathways')
-    ax.set_title('Total Incomplete Pathways by ICB Name')
-    plt.xticks(rotation=90)  # Rotate the x-axis labels to show them more clearly
+# Create a vertical bar chart using Matplotlib
+fig, ax = plt.subplots()
+ax.bar(df_sorted['Treatment Function'], df_sorted['Total number of incomplete pathways'])
+ax.set_xlabel('Treatment Function')
+ax.set_ylabel('Total number of incomplete pathways')
+ax.set_title('Total Incomplete Pathways by Treatment Function for NHS SUSSEX INTEGRATED CARE BOARD')
+plt.xticks(rotation=90)  # Rotate the x-axis labels to show them more clearly
 
-    # Display the chart
-    st.pyplot(fig)
+# Display the chart
+st.pyplot(fig)
