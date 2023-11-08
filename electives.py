f69b647fe5ae5c57b5e9250d52c0fbf134fb31c4
Python_explorer <70327477+Python-explorer@users.noreply.github.com>
2023-11-08 21:13:20 +0000

    Update electives.py

--- electives.py
+++ electives.py
@@ -24,19 +24,25 @@ df['ICB Name'] = df['ICB Name'].str.replace('INTEGRATED CARE BOARD', 'ICB')
 # Streamlit page title
 st.title('ICB Electives Dashboard Demo')
 
-# Dropdown to select the column for bar values
+# Sidebar dropdown for selecting the value column
 selected_value_column = st.sidebar.selectbox(
     'Select the column for bar values',
     options=['Total number of incomplete pathways', 'Total 65 plus weeks']
 )
 
-# Dropdown to select the criteria for rows
+# Sidebar dropdown for selecting the Treatment Function
 selected_treatment_function = st.sidebar.selectbox(
     'Select the Treatment Function',
     options=df['Treatment Function'].unique()
 )
 
-# Filter the DataFrame to exclude 'NHS ENGLAND' from 'ICB Name' and for the selected 'Treatment Function'
+# Sidebar dropdown for ICB focus
+selected_icb_focus = st.sidebar.selectbox(
+    'ICB focus',
+    options=['None'] + list(df['ICB Name'].unique())
+)
+
+# Filter the DataFrame to exclude 'NHS ENGLAND' from 'ICB Name'
 df_filtered = df[(df['ICB Name'] != 'NHS ENGLAND') & (df['Treatment Function'] == selected_treatment_function)]
 
 # Group by 'ICB Name' and sum the selected value column
@@ -47,19 +53,33 @@ df_sorted = df_grouped.sort_values(by=selected_value_column, ascending=True)
 
 # Calculate quartiles for the selected value column
 q1 = df_sorted[selected_value_column].quantile(0.25)
-q2 = df_sorted[selected_value_column].quantile(0.50)
 q3 = df_sorted[selected_value_column].quantile(0.75)
 
 # Assign colors based on quartiles
-colors = ['green' if x < q1 else 'orange' if x < q3 else 'red' for x in df_sorted[selected_value_column]]
+colors = []
+for x in df_sorted[selected_value_column]:
+    if x < q1:
+        colors.append('green')
+    elif x < q3:
+        colors.append('orange')
+    else:
+        colors.append('red')
 
-# Create a vertical bar chart using Matplotlib
+# Create a vertical bar chart with increased figure size for better readability
 fig, ax = plt.subplots(figsize=(15, 8))
-ax.bar(df_sorted['ICB Name'], df_sorted[selected_value_column], color=colors)
+
+# Plot each bar individually to set colors, including the ICB focus
+for i, (icb_name, value) in enumerate(zip(df_sorted['ICB Name'], df_sorted[selected_value_column])):
+    color = 'paleblue' if icb_name == selected_icb_focus and selected_icb_focus != 'None' else colors[i]
+    ax.bar(icb_name, value, color=color)
+
 ax.set_xlabel('ICB Name')
 ax.set_ylabel(selected_value_column)
 ax.set_title(f'{selected_value_column} by ICB Name for {selected_treatment_function}')
-plt.xticks(rotation=90)  # Rotate the x-axis labels to show them more clearly
+plt.xticks(rotation=45, ha='right')  # Adjust the rotation and alignment of x-axis labels
+
+# Improve the layout to prevent label overlap
+plt.tight_layout()
 
 # Display the chart
 st.pyplot(fig)
