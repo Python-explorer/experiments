6f0ae74963bb89203c9891cc6ec0628eae3019b6
Python_explorer <70327477+Python-explorer@users.noreply.github.com>
2023-11-08 00:19:15 +0000

    Update electives.py

--- electives.py
+++ electives.py
@@ -4,14 +4,6 @@ import pandas as pd
 # Function to load CSV data from a URL
 @st.cache_resource
 def load_data(url):
-    try:
-        # The 'on_bad_lines' parameter is used to handle problematic lines.
-        # Set it to 'warn' to issue a warning and skip them.
-        data = pd.read_csv(url, on_bad_lines='warn')  # Make sure this line is correct
-    except Exception as e:
-        st.error(f"Failed to load data: {e}")
-        return pd.DataFrame()  # Ensure this return statement is within the except block
-    return data  # This should be outside the try-except block
 
 # URL to the CSV file
 data_url = 'https://github.com/Python-explorer/experiments/blob/main/ElectiveDataICB.csv'
